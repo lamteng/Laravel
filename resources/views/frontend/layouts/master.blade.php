ceab70201691b05041ff24cb0214cf4055477658
Lam Teng <lamteng@hotmail.com>
2020-11-19 23:09:03 +0000

    minor update in blade.php

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="zh-TW">
 <head>
-    <title>Laravel main page - @yield('title')</title>
+    <title>@yield('title')</title>
 </head>
 <body>
-    @include('frontend.layouts.header')
+    @include('frontend.layouts.topics')
     @include('frontend.layouts.navbar')
     @yield('content')
     @include('frontend.layouts.footer')
 </body>
+
 </html>
